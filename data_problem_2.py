5ec77dbbbe02db65982353520b7c32c1fbfa62b0
James Glenister <jglen419@gmail.com>
2019-03-29 17:00:31 -0400

    completed data problem 2

--- data_problem_2.py
+++ data_problem_2.py
@@ -6,3 +6,32 @@
 #a) X = 240, Y = 320, P1 = $40, P2 = $30, H1 = 2, H2 = 1
 #b) X = 300, Y = 380, P1 = $70, P2 = $45, H1 = 3, H2 = 1
 #c) X = 180, Y = 420, P1 = $65, P2 = $55, H1 = 3, H2 = 2
+
+from scipy.optimize import linprog
+
+def createMatrix(X,Y,p1,p2,h1,h2):
+    c = [-p1,-p2]
+    A = [[1,1],[h1,h2]]
+    b = [X,Y]
+    
+    return [c,A,b]
+
+def optimizeProblem(c,A,b):
+
+    result = linprog(c,A,b)
+    acres = result.get('x')
+
+    print(str(acres[0]) + ' acres of corn')
+    print(str(acres[1]) + ' acres of oats')
+    print('')
+
+x = [240.,300.,180.]
+y = [320.,380.,420.]
+p1 = [40.,70.,650.]
+p2 = [30.,45.,55.]
+h1 = [2.,3.,3.]
+h2 = [1.,1.,2.]
+
+for i in range(len(x)):
+    setup = createMatrix(x[i],y[i],p1[i],p2[i],h1[i],h2[i])
+    optimizeProblem(setup[0],setup[1],setup[2])
