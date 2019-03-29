552de7e987c551b401f7245c4288aa492eba7634
James Glenister <jglen419@gmail.com>
2019-03-28 21:10:22 -0400

    completed modeling problem 3

--- modelling_problem_3.py
+++ modelling_problem_3.py
@@ -1,2 +1,21 @@
 #Given the differential equation f'(x) = x^x, write a function that uses Euler's 
 #method to approximate the value of f(x1) given an initial condition (x0, f(x0)) and the value of x1.
+
+def fprime(x):
+    return x**x
+
+def euler_method(x0,fx0,x1):
+    
+    dx = 0.00001
+    x = x0
+    fx = fx0
+
+    while (x <= x1):
+       fx += fprime(x) * dx
+       x += dx 
+    
+    return fx
+
+
+#test
+print(euler_method(0,0,2))
