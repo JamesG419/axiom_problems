e68b3d2670a2515a1a851ac09238d9aafe927b61
James Glenister <jglen419@gmail.com>
2019-03-28 21:53:12 -0400

    completed modeling problem 2

--- modelling_problem_2.py
+++ modelling_problem_2.py
@@ -1,2 +1,10 @@
 #Write a function to calculate the pressure drop in a pipe given the flow velocity, density, viscosity, and pipe diameter,
 #and pipe length, assuming the Reynold’s number is less than 2300. 
+
+#Re < 2300: Laminar flow
+
+def pressure_calc(velocity,density,viscosity,diameter,length):
+
+    pressure_drop = 32 * viscosity *  length * velocity / diameter**2
+    
+    return pressure_drop
